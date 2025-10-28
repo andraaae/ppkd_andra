cfd80a0c46be9459b39c918b86d2129f56c8f5bf
andraaae <andrariedwan@gmail.com>
2025-10-28 20:58:37 +0700

    register on going

---
+++ lib/agreen/view/register_agreen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class RegisterAgreen extends StatefulWidget {
+  const RegisterAgreen({super.key});
+
+  @override
+  State<RegisterAgreen> createState() => _RegisterAgreenState();
+}
+
+class _RegisterAgreenState extends State<RegisterAgreen> {
+  final formKey = GlobalKey<FormState>();
+    final TextEditingController nameController = TextEditingController();
+    final TextEditingController phoneController = TextEditingController();
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+    bool isVisible = false;
+    bool _obscurePass = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffA3DC9A),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(25),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+
+              children: [
+                //Image.asset(''),
+                SizedBox(height: 20),
+                Text('Register now', style: TextStyle(fontSize: 20),
+                ),
+
+                //menambahkan username, email, nomor telephone, dan password
+                SizedBox(height: 29),
+                Text('Username'),
+                TextField(decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(12),
+                
+                ),
+                ),
+                )
+              ],
+            )),
+        ),
+      ),
+    );
+  }
+}
